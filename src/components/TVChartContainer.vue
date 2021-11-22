9328707aa78490cc4b58a8b01714cf495fc42049
SenkOne123 <senkone0123@gmail.com>
2021-11-22 22:01:43 +0500

    new html buttons

--- src/components/TVChartContainer.vue
+++ src/components/TVChartContainer.vue
@@ -1,239 +1,236 @@
 <template>
   <div>
-    <div class="TVChartContainer" :id="container" />
+    <div class="button_box">
+      <button @click="" class="button">Play</button>
+      <button @click="longPosButtonClick" class="button">Long Pos</button>
+      <button @click="shortPosButtonClick" class="button">Short Pos</button>
+      <button @click="arrowUpButtonClick" class="button">Icon Up</button>
+      <button @click="arrowDownButtonClick" class="button">Icon Down</button>
+      <button @click="textButtonClick" class="button">Text</button>
+      <button @click="lineButtonClick" class="button">Line</button>
+      <button @click="saveButtonClick" class="button">Save</button>
+      <button @click="loadButtonClick" class="button">Load</button>
+    </div>
+    <div class="TVChartContainer" :id="container"/>
   </div>
 </template>
 
 <script>
-  import api from './api'
-  import {getKlines, subscribeKline} from "./api/helpers";
+import api from './api'
+import {getKlines} from "./api/helpers";
 
-  export default {
-    name: 'TVChartContainer',
-    props: {
-      symbol: {
-        default: 'AAPL',
-        type: String,
-      },
-      interval: {
-        default: 'D',
-        type: String,
-      },
-      container: {
-        default: 'tv_chart_container',
-        type: String,
-      },
-      datafeedUrl: {
-        default: 'https://demo_feed.tradingview.com',
-        type: String,
-      },
-      libraryPath: {
-        default: '/charting_library/charting_library/',
-        type: String,
-      },
-      chartsStorageUrl: {
-        default: 'https://saveload.tradingview.com',
-        type: String,
-      },
-      chartsStorageApiVersion: {
-        default: '1.2',
-        type: String,
-      },
-      clientId: {
-        default: 'tradingview.com',
-        type: String,
-      },
-      userId: {
-        default: 'public_user_id',
-        type: String,
-      },
-      fullscreen: {
-        default: false,
-        type: Boolean,
-      },
-      autosize: {
-        default: true,
-        type: Boolean,
-      },
-      studiesOverrides: {
-        type: Object,
-      }
+export default {
+  name: 'TVChartContainer',
+  props: {
+    symbol: {
+      default: 'AAPL',
+      type: String,
     },
-    tvWidget: null,
-    widgets: null,
-    savedChart: null,
-    bars: null,
-    methods: {
-      handlePlayButtonClick() {
-        this.widgets.onChartReady(() => {
-          this.widgets.activeChart().createMultipointShape(
-                [{time: 1637192940}, {time: 1637192940 + 60*15}],
-                {
-                  shape: 'long_position',
-                  overrides: {
-                    profitLevel: 20,
-                    stopLevel: 20,
-                  },
-                })
-        })
-      }
+    interval: {
+      default: 'D',
+      type: String,
     },
-    mounted() {
-      const widgetOptions = {
-        symbol: 'EURUSD',
-        datafeed: api,
-        interval: '1',
-        container: 'tv_chart_container',
-        library_path: '/charting_library/charting_library/',
-        locale: 'en',
-        disabled_features: ['use_localstorage_for_settings'],
-        fullscreen: false,
-        autosize: true,
-        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
-      }
-      this.tvWidget = new TradingView.widget(widgetOptions)
-      this.widgets = new TradingView.widget(widgetOptions)
-
-      let count = 0
-
-      getKlines({
-        symbol: 'EURUSD',
-        interval: '1',
-        from: new Date(0),
-        to: Date.now(),
-        limit: 1000,
-      }).then(res => {
-        this.bars = res.slice(0, 799)
-      })
-
-      this.widgets.onChartReady(() => {
-
-        this.widgets.headerReady().then(() => {
-          let playButton = this.widgets.createButton()
-          let longPos = this.widgets.createButton()
-          let shortPos = this.widgets.createButton()
-          let iconUp = this.widgets.createButton()
-          let iconDown = this.widgets.createButton()
-          let text = this.widgets.createButton()
-          let line = this.widgets.createButton()
-          let saveButton = this.widgets.createButton()
-          let loadButton = this.widgets.createButton()
-          playButton.textContent = 'Play'
-          longPos.textContent = 'Long Pos'
-          shortPos.textContent = 'Short Pos'
-          iconUp.textContent = 'Icon Up'
-          iconDown.textContent = 'Icon Down'
-          text.textContent = 'Text'
-          line.textContent = 'Line'
-          saveButton.textContent = 'Save'
-          loadButton.textContent = 'Load'
-          playButton.addEventListener('click', () => {
-
-          })
-          longPos.addEventListener('click', () => {
-            this.widgets.activeChart().createMultipointShape(
-                [{time: this.bars[798].time/1000}, {time: this.bars[798].time/1000 + 60*30}],
-                {
-                  shape: 'long_position',
-                  overrides: {
-                    profitLevel: 20,
-                    stopLevel: 20,
-                  },
-                })
-          })
-          shortPos.addEventListener('click', () => {
-            this.widgets.activeChart().createMultipointShape(
-                [{time: this.bars[798].time/1000}, {time: this.bars[798].time/1000 + 60*30}],
-                {
-                  shape: 'short_position',
-                  overrides: {
-                    profitLevel: 20,
-                    stopLevel: 20,
-                  },
-                })
-          })
-          iconDown.addEventListener('click', () => {
-            this.widgets.activeChart().createShape(
-                {time: this.bars[500].time/1000, price: 1.129 },
-                {
-                  shape: 'arrow_down'
-                }
-            )
-          })
-          iconUp.addEventListener('click', () => {
-            this.widgets.activeChart().createShape(
-                {time: this.bars[500].time/1000, price: 1.128 },
-                {
-                  shape: 'arrow_up'
-                }
-            )
-          })
-          line.addEventListener('click', () => {
-            this.widgets.activeChart().createMultipointShape(
-                [{time: this.bars[700].time/1000}],
-                {
-                  shape: 'vertical_line',
-                  overrides: {
-                    profitLevel: 20,
-                    stopLevel: 20,
-                  },
-                })
-          })
-          text.addEventListener('click', () => {
-            this.widgets.activeChart().createShape(
-                {time: this.bars[798].time/1000, price: 1.129 },
-                {
-                  shape: 'text',
-                  overrides: {
-                    showLabel: true,
-                    textSize: 16,
-                  },
-                  text: 'Sample Text'
-                }
-            )
-          })
-          saveButton.addEventListener('click', () => {
-            this.tvWidget.save(data => {
-              this.savedChart = data
+    container: {
+      default: 'tv_chart_container',
+      type: String,
+    },
+    datafeedUrl: {
+      default: 'https://demo_feed.tradingview.com',
+      type: String,
+    },
+    libraryPath: {
+      default: '/charting_library/charting_library/',
+      type: String,
+    },
+    chartsStorageUrl: {
+      default: 'https://saveload.tradingview.com',
+      type: String,
+    },
+    chartsStorageApiVersion: {
+      default: '1.2',
+      type: String,
+    },
+    clientId: {
+      default: 'tradingview.com',
+      type: String,
+    },
+    userId: {
+      default: 'public_user_id',
+      type: String,
+    },
+    fullscreen: {
+      default: false,
+      type: Boolean,
+    },
+    autosize: {
+      default: true,
+      type: Boolean,
+    },
+    studiesOverrides: {
+      type: Object,
+    }
+  },
+  tvWidget: null,
+  widgets: null,
+  savedChart: null,
+  bars: null,
+  play: false,
+  methods: {
+    handlePlayButtonClick() {
+    },
+    longPosButtonClick() {
+        this.widgets.activeChart().createMultipointShape(
+            [{time: this.bars[798].time / 1000}, {time: this.bars[798].time / 1000 + 60 * 30}],
+            {
+              shape: 'long_position',
+              overrides: {
+                profitLevel: 20,
+                stopLevel: 20,
+              },
             })
-            localStorage.setItem('widgets', JSON.stringify(this.savedChart))
+      },
+    shortPosButtonClick() {
+      this.widgets.activeChart().createMultipointShape(
+          [{time: this.bars[798].time / 1000}, {time: this.bars[798].time / 1000 + 60 * 30}],
+          {
+            shape: 'short_position',
+            overrides: {
+              profitLevel: 20,
+              stopLevel: 20,
+            },
           })
-          loadButton.addEventListener('click', () => {
-            this.widgets.load(JSON.parse(localStorage.getItem('widgets')))
-          })
-
-        })
-
-          this.widgets.subscribe('drawing_event', (id, type) => {
-            console.log(id,type)
-          })
-
-        //
-        //   widget.activeChart().getShapeById(arrowIcon).setProperties({
-        //     text: 'AAAAA'
-        //   })
-        //
-        //   widget.activeChart().getAllShapes().forEach(({ id, name }) => console.log(id, name))
-        // })
-      })
-      this.tvWidget.onChartReady(() => {
-
-      })
-
     },
-    destroyed() {
-      if (this.tvWidget !== null) {
-        this.tvWidget.remove()
-        this.tvWidget = null
-      }
+    arrowUpButtonClick() {
+      this.widgets.activeChart().createShape(
+          {time: this.bars[500].time / 1000, price: this.bars[500].close + 0.00090},
+          {
+            shape: 'arrow_up'
+          }
+      )
+    },
+    arrowDownButtonClick() {
+      this.widgets.activeChart().createShape(
+          {time: this.bars[600].time / 1000, price: this.bars[700].close + 0.00090},
+          {
+            shape: 'arrow_down'
+          }
+      )
+    },
+    textButtonClick() {
+      this.widgets.activeChart().createShape(
+          {time: this.bars[798].time / 1000, price: 1.129},
+          {
+            shape: 'text',
+            overrides: {
+              showLabel: true,
+              textSize: 16,
+            },
+            text: 'Sample Text'
+          }
+      )
+    },
+    lineButtonClick() {
+      this.widgets.activeChart().createMultipointShape(
+          [{time: this.bars[700].time / 1000}],
+          {
+            shape: 'vertical_line',
+            overrides: {
+              profitLevel: 20,
+              stopLevel: 20,
+            },
+          })
+    },
+    saveButtonClick() {
+      this.tvWidget.save(data => {
+        this.savedChart = data
+      })
+      localStorage.setItem('widgets', JSON.stringify(this.savedChart))
+    },
+    loadButtonClick() {
+      this.widgets.load(JSON.parse(localStorage.getItem('widgets')))
+    }
+  },
+  mounted() {
+    const widgetOptions = {
+      symbol: 'EURUSD',
+      datafeed: api,
+      interval: '1',
+      container: 'tv_chart_container',
+      library_path: '/charting_library/charting_library/',
+      locale: 'en',
+      disabled_features: ['use_localstorage_for_settings'],
+      fullscreen: false,
+      autosize: true,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    }
+    this.tvWidget = new TradingView.widget(widgetOptions)
+    this.widgets = new TradingView.widget(widgetOptions)
+
+    let count = 0
+
+    getKlines({
+      symbol: 'EURUSD',
+      interval: '1',
+      from: new Date(0),
+      to: Date.now(),
+      limit: 1000,
+    }).then(res => {
+      this.bars = res.slice(0, 799)
+    })
+
+    this.widgets.onChartReady(() => {
+      this.widgets.subscribe('drawing_event', (id, type) => {
+        console.log(id, type)
+      })
+
+    })
+    this.tvWidget.onChartReady(() => {
+    })
+
+  },
+  destroyed() {
+    if (this.tvWidget !== null) {
+      this.tvWidget.remove()
+      this.tvWidget = null
     }
   }
+}
 </script>
 
 <style>
-  .TVChartContainer {
-    position: absolute;
-    width: 100%;
-    height: 95%;
-  }
+.TVChartContainer {
+  position: absolute;
+  width: 100%;
+  height: 95%;
+}
+
+.button_box {
+  display: flex;
+  margin-top: 15px;
+  margin-left: 45px;
+}
+
+.button {
+  font-family: "Roboto Thin", sans-serif;
+  margin-left: 10px;
+  color: #ffffff;
+  background-color: #000000;
+  border-radius: 4px;
+  border-width: 0;
+  min-width: 100px;
+  min-height: 30px;
+}
+
+.button:hover {
+  font-family: "Roboto Thin", sans-serif;
+  margin-left: 10px;
+  border-radius: 4px;
+  color: #ffffff;
+  background-color: #555555;
+  box-shadow: #333333;
+  border-width: 0;
+  min-width: 100px;
+  min-height: 30px;
+}
+
 </style>
--- src/components/api/index.js
+++ src/components/api/index.js
@@ -6,15 +6,15 @@
  * Stream implementation: https://github.com/tradingview/charting-library-tutorial/blob/master/documentation/streaming-implementation.md
  */
 
-import { getExchangeServerTime, getSymbols, getKlines, subscribeKline, unsubscribeKline, checkInterval } from './helpers'
+import {getExchangeServerTime, getSymbols, getKlines, subscribeKline, unsubscribeKline, checkInterval} from './helpers'
 
 const configurationData = {
-	supports_marks: false,
-	supports_timescale_marks: false,
-	supports_time: true,
-	supported_resolutions: [
-		'1', '3', '5', '15', '30', '60', '120', '240', '1D', '3D', '1W', '1M'
-	]
+    supports_marks: false,
+    supports_timescale_marks: false,
+    supports_time: true,
+    supported_resolutions: [
+        '1', '3', '5', '15', '30', '60', '120', '240', '1D', '3D', '1W', '1M'
+    ]
 };
 let drawed = false
 let intervalChange = ''
@@ -22,134 +22,132 @@ let newBar = []
 let play = false
 
 export default {
-	// get a configuration of your datafeed (e.g. supported resolutions, exchanges and so on)
-	onReady: (callback) => {
-		console.log('[onReady]: Method call');
-		setTimeout(() => callback(configurationData)) // callback must be called asynchronously.
-	},
+    // get a configuration of your datafeed (e.g. supported resolutions, exchanges and so on)
+    onReady: (callback) => {
+        console.log('[onReady]: Method call');
+        setTimeout(() => callback(configurationData)) // callback must be called asynchronously.
+    },
 
-	 // NO need if not using search
-	searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
-		let symbols = [{
-				symbol: "EURUSD",
-				full_name: "EURUSD", // e.g. BTCE:BTCUSD
-				description: "EUR to USD with using 1m candles",
-				exchange: "EURUSD",
-				ticker: "EURUSD",
-				type: "stock"
-			}]
-		// getSymbols().then(res => {
-		// 	symbols = res.filter(symbol => symbol.symbol === 'EURUSD'|'BTCUSDT')
-		// 	console.log(symbols)
-		// })
-		onResultReadyCallback(symbols)
-	},
-
-
-	// retrieve information about a specific symbol (exchange, price scale, full symbol etc.)
-	resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
-		console.log('[resolveSymbol]: Method call', symbolName);
-
-		const comps = symbolName.split(':')
-		symbolName = (comps.length > 1 ? comps[1] : symbolName).toUpperCase()
+    // NO need if not using search
+    searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
+        let symbols = [{
+            symbol: "EURUSD",
+            full_name: "EURUSD", // e.g. BTCE:BTCUSD
+            description: "EUR to USD with using 1m candles",
+            exchange: "EURUSD",
+            ticker: "EURUSD",
+            type: "stock"
+        }]
+        // getSymbols().then(res => {
+        // 	symbols = res.filter(symbol => symbol.symbol === 'EURUSD'|'BTCUSDT')
+        // 	console.log(symbols)
+        // })
+        onResultReadyCallback(symbols)
+    },
 
 
-		// need for pricescale()
-		function pricescale(symbol) {
-			for (let filter of symbol.filters) {
-				if (filter.filterType == 'PRICE_FILTER') {
-					return Math.round(1 / parseFloat(filter.tickSize))
-				}
-			}
-			return 1
-		}
+    // retrieve information about a specific symbol (exchange, price scale, full symbol etc.)
+    resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
+        console.log('[resolveSymbol]: Method call', symbolName);
 
-		const symbolInfo = (symbol) => ({
-			name: symbol.symbol,
-			description: symbol.baseAsset + ' / ' + symbol.quoteAsset,
-			ticker: symbol.symbol,
-			//exchange: 'Binance',
-			//listed_exchange: 'Binance',
-			//type: 'crypto',
-			session: '24x7',
-			minmov: 1,
-			pricescale: 100000, //pricescale(symbol), // 	or 100
-			timezone: 'UTC',
-			has_intraday: true,
-			has_daily: true,
-			has_weekly_and_monthly: true,
-			currency_code: symbol.quoteAsset,
-		})
-
-		// Get symbols
-		getSymbols().then(symbols => {
-			const symbol = symbols.find(i => i.symbol == symbolName)
-			return symbol ? onSymbolResolvedCallback(symbolInfo(symbol)) : onResolveErrorCallback('[resolveSymbol]: symbol not found')
-		})
-
-	},
+        const comps = symbolName.split(':')
+        symbolName = (comps.length > 1 ? comps[1] : symbolName).toUpperCase()
 
 
+        // need for pricescale()
+        function pricescale(symbol) {
+            for (let filter of symbol.filters) {
+                if (filter.filterType == 'PRICE_FILTER') {
+                    return Math.round(1 / parseFloat(filter.tickSize))
+                }
+            }
+            return 1
+        }
 
-	// get historical data for the symbol
-	// https://github.com/tradingview/charting_library/wiki/JS-Api#getbarssymbolinfo-resolution-periodparams-onhistorycallback-onerrorcallback
-	getBars: async (symbolInfo, interval, periodParams, onHistoryCallback, onErrorCallback) => {
-		console.log('[getBars] Method call', symbolInfo, interval)
+        const symbolInfo = (symbol) => ({
+            name: symbol.symbol,
+            description: symbol.baseAsset + ' / ' + symbol.quoteAsset,
+            ticker: symbol.symbol,
+            //exchange: 'Binance',
+            //listed_exchange: 'Binance',
+            //type: 'crypto',
+            session: '24x7',
+            minmov: 1,
+            pricescale: 100000, //pricescale(symbol), // 	or 100
+            timezone: 'UTC',
+            has_intraday: true,
+            has_daily: true,
+            has_weekly_and_monthly: true,
+            currency_code: symbol.quoteAsset,
+        })
 
-		if (!checkInterval(interval)) {
-			return onErrorCallback('[getBars] Invalid interval')
-		}
+        // Get symbols
+        getSymbols().then(symbols => {
+            const symbol = symbols.find(i => i.symbol == symbolName)
+            return symbol ? onSymbolResolvedCallback(symbolInfo(symbol)) : onResolveErrorCallback('[resolveSymbol]: symbol not found')
+        })
 
-		const dateFrom = new Date(0)
+    },
 
-		let klines = await getKlines({
-			symbol: symbolInfo.name, interval, from: new Date(0), to: Date.now(), limit: 1000
-		})
 
-		newBar = klines.slice(800,klines.length)
-		const historicLines = klines.slice(0,799)
+    // get historical data for the symbol
+    // https://github.com/tradingview/charting_library/wiki/JS-Api#getbarssymbolinfo-resolution-periodparams-onhistorycallback-onerrorcallback
+    getBars: async (symbolInfo, interval, periodParams, onHistoryCallback, onErrorCallback) => {
+        console.log('[getBars] Method call', symbolInfo, interval)
 
-		console.log(historicLines)
+        if (!checkInterval(interval)) {
+            return onErrorCallback('[getBars] Invalid interval')
+        }
 
-		if (intervalChange === interval) {
-			drawed = true
-			intervalChange = interval
-		}
-		else {
-			drawed = false
-			intervalChange = interval
-		}
-		if (klines.length > 0 && !drawed) {
-			onHistoryCallback(historicLines)
-		}
+        const dateFrom = new Date(0)
 
-		onErrorCallback('Klines data error')
+        let klines = await getKlines({
+            symbol: symbolInfo.name, interval, from: new Date(0), to: Date.now(), limit: 1000
+        })
 
-	},
-	// subscription to real-time updates
-	subscribeBars: (symbolInfo, interval, onRealtimeCallback, subscribeUID, onResetCacheNeededCallback) => {
-		console.log('[subscribeBars]: Method call with subscribeUID:', subscribeUID);
+        newBar = klines.slice(800, klines.length)
+        const historicLines = klines.slice(0, 799)
 
-		if (play) {
-			for (let i = 0; i < newBar.length; i++) {
-			setTimeout(function timer() {
-				onRealtimeCallback(newBar[i])
-			}, i * 1000)
-			}
-		}
+        console.log(historicLines)
 
-		subscribeKline({ symbol: symbolInfo.name, interval, uniqueID: subscribeUID, }, cb => {
-		})
-	},
-	unsubscribeBars: (subscriberUID) => {
-		console.log('[unsubscribeBars]: Method call with subscriberUID:', subscriberUID);
-		unsubscribeKline(subscriberUID)
-	},
-	getServerTime: (callback) => {
-		getExchangeServerTime().then(time => {
-			callback(Math.floor(time / 1000))
-		}).catch(err => {
-			console.error(err)
-		})
-	}
+        if (intervalChange === interval) {
+            drawed = true
+            intervalChange = interval
+        } else {
+            drawed = false
+            intervalChange = interval
+        }
+        if (klines.length > 0 && !drawed) {
+            onHistoryCallback(historicLines)
+        }
+
+        onErrorCallback('Klines data error')
+
+    },
+    // subscription to real-time updates
+    subscribeBars: (symbolInfo, interval, onRealtimeCallback, subscribeUID, onResetCacheNeededCallback) => {
+        console.log('[subscribeBars]: Method call with subscribeUID:', subscribeUID);
+
+        if (play) {
+            for (let i = 0; i < newBar.length; i++) {
+                setTimeout(function timer() {
+                    onRealtimeCallback(newBar[i])
+                }, i * 1000)
+            }
+        }
+
+        subscribeKline({symbol: symbolInfo.name, interval, uniqueID: subscribeUID,}, cb => {
+        })
+    },
+    unsubscribeBars: (subscriberUID) => {
+        console.log('[unsubscribeBars]: Method call with subscriberUID:', subscriberUID);
+        unsubscribeKline(subscriberUID)
+    },
+    getServerTime: (callback) => {
+        getExchangeServerTime().then(time => {
+            callback(Math.floor(time / 1000))
+        }).catch(err => {
+            console.error(err)
+        })
+    }
 };
